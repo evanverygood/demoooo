e01bf0487d066ff03148577e47a8eb3ec5ec6ba6
seconed <evanverygood@sina.com>
2019-12-17 19:38:54 +0800

    难受哇

---
+++ src/main/java/com/aba/demoooo/controller/StudentApiContoroller.java
@@ -0,0 +1,12 @@
+package com.aba.demoooo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class StudentApiContoroller {
+
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }    
+}
---
+++ src/test/java/com/aba/demoooo/DemooooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aba.demoooo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemooooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
